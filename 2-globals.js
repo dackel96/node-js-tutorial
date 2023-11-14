3810dbe26350e3663f29e45a8146a0418762c61c
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-15 01:04:23 +0200

    modules

---
+++ 2-globals.js
@@ -0,0 +1,21 @@
+//Globals - No Window!
+
+//__dirname  - path to current directory.
+//__filename - file name.
+// require   - function to use modules (CommonJS).
+// module    - info about current module (file).
+// process   - info about environment where the program is being executed.
+
+console.log(__dirname);
+
+// setInterval();
+
+// setInterval(() => {
+//   console.log('hello world!');
+// }, 1000);
+
+// setTimeout();
+
+// setTimeout(() => {
+//   console.log('Hello World!');
+// }, 5000);
