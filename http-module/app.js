57bcecc2ba0ead5eb80951d0d1bf69cdfcd8fc0f
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-18 10:36:23 +0200

    http in node

--- http-module/app.js
+++ http-module/app.js
@@ -2,8 +2,17 @@
 const http = require('http');
 //two props we have request and response.
 const server = http.createServer((req, res) => {
-  res.write('Welcome to my home page');
-  res.end();
+  if (req.url === '/') {
+    res.end('Welcome to my home page');
+  }
+  if (req.url === '/about') {
+    res.end('short long story');
+  }
+  res.end(`
+  <h1>Oops!</h1>
+  <p>error</p>
+  <a href="/">back to home page</a>
+  `);
 });
 
 server.listen(5000);
